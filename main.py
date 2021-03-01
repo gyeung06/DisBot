2481b4ba95bb4e865a640b88a967d0618030229f
Gabriel Yeung <gabriel.yeung719@gmail.com>
2021-03-01 03:28:55 -0800

    Built twitch link command Upload works Calling link works Stored in json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import discord
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 
@@ -16,9 +17,9 @@ def write_json(data, filename='test.json'):
         json.dump(data, outfile)
 
 
-def get_new_id(id):
+def get_new_id(uid):
     inp = {
-        "id": id,
+        "id": uid,
         "link": "",
         "groups": ""
     }
@@ -31,9 +32,12 @@ async def on_command_error(ctx, error):
         await ctx.send('I have no idea what you want')
 
 
-
-@bot.command()
-async def iTwitch(ctx, link):
+@bot.command(
+    help="Call this with your twitch link, or someone else's not my issue really and the link will be"
+         "associated with your account to call with ~mTwitch",
+    brief="Associates your account with your twitch link\n"
+)
+async def itwitch(ctx, link):
     file_users = open("test.json")
     users = json.load(file_users)
     file_users.close()
@@ -48,6 +52,7 @@ async def iTwitch(ctx, link):
             if str(ctx.author.id) == str(keyval['id']):
                 keyval['link'] = link
                 user_found = True
+                break
 
         if not user_found:
             new_entry = get_new_id(ctx.author.id)
@@ -55,19 +60,48 @@ async def iTwitch(ctx, link):
             users['members'].append(new_entry)
 
         write_json(users)
-        await ctx.send('I got you homie')
+        await ctx.send('Got it')
     else:
         await ctx.send('Not a twitch link')
 
 
-@bot.command()
-async def mTwitch(ctx):
+@bot.command(
+    help="When called with no mentions will bring up your own twitch link -- "
+         "If used with a mention will use that users associated link instead if they have one",
+    brief="Calls either your twitch link or whoever you mention\n"
+)
+async def mtwitch(ctx, members: commands.Greedy[discord.Member]):
+    print('hu')
+    if len(members) == 0:
+        name = ctx.author.id
+    elif len(members) == 1:
+        name = members[0].id
+    else:
+        await ctx.send('One name at a time pal')
+        return
+
+    file_users = open("test.json")
+    users = json.load(file_users)
+    file_users.close()
+
+    for keyval in users['members']:
+        if str(name) == str(keyval['id']):
+            if len(keyval['link']) < 23:
+                await ctx.send('Link not set yet')
+            else:
+                await ctx.send(keyval['link'])
+                return
+
+    await ctx.send('Link not set yet')
 
 
-
-@bot.command()
-async def bestWep(ctx):
+@bot.command(
+    help="Just do it",
+    brief="Try it\n"
+)
+async def bestwep(ctx):
     await ctx.send('DEBATE CLUB')
+    await ctx.send(file=discord.File('TrulyTheBest.png'))
 
 
 bot.run(TOKEN)
